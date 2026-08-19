edb7b7ec07cf7948cee86d584ea6583808a566d0
Luoluozi110 <775225929@qq.com>
2026-08-20 02:32:32 +0800

    deploy: keep angle control in narrow desktop views

--- js/ui/app.js
+++ js/ui/app.js
@@ -5,7 +5,7 @@
  */
 import { loadConfig, configSource, applyProjectOverride, loadCloudUrl } from '../engine/config.js?v=20260819strategyv2';
 import { Game } from '../engine/game.js?v=20260819strategyv2';
-import { BoardView } from './board.js?v=20260820angledefault';
+import { BoardView } from './board.js?v=20260820anglecontrol2';
 import { Hud, radarSVG } from './hud.js?v=20260820uioverlap';
 import { Modals } from './modals.js?v=20260819strategyv2';
 import { BattleStage } from './battle.js?v=20260819strategyv2';
